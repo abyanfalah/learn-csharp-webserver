4f3b4c49c9b2ae27fdd8bcdd8b39ca2185e44d5f
Abyan Falah <abyanf410@gmail.com>
2022-11-08 16:48:34 +0700

    refactor

--- Program.cs
+++ Program.cs
@@ -2,44 +2,48 @@
 using System.Net;
 
 
-Console.Clear();
-
-using var listener = new HttpListener();
-const string host = "http://localhost";
-const string port = "8000";
-const string uri = host + ":" + port + "/";
-listener.Prefixes.Add(uri);
-
-listener.Start();
-
-Console.WriteLine($"listeting to port {port}");
-
-while (true)
+internal class Program
 {
-	HttpListenerContext ctx = listener.GetContext();
-	HttpListenerRequest req = ctx.Request;
+	private static void Main(string[] args)
+	{
+		Console.Clear();
 
-	using HttpListenerResponse res = ctx.Response;
-	// res.Headers.Set("Content-type", "application/json");
-	res.Headers.Set("Content-type", "text/plain");
+		using var listener = new HttpListener();
+		const string host = "http://localhost";
+		const string port = "8000";
+		const string uri = host + ":" + port + "/";
+		listener.Prefixes.Add(uri);
 
-	res.StatusCode = (int)HttpStatusCode.OK;
-	res.StatusDescription = "OK";
+		listener.Start();
 
+		Console.WriteLine($"listeting to port {port}");
 
-	string data = "Hello World";
-	byte[] buffer = Encoding.UTF8.GetBytes(data);
-	res.ContentLength64 = buffer.Length;
+		while (true)
+		{
+			HttpListenerContext ctx = listener.GetContext();
+			HttpListenerRequest req = ctx.Request;
 
-	using Stream ros = res.OutputStream;
-	ros.Write(buffer, 0, buffer.Length);
+			using HttpListenerResponse res = ctx.Response;
+			// res.Headers.Set("Content-type", "application/json");
+			res.Headers.Set("Content-type", "text/plain");
 
+			res.StatusCode = (int)HttpStatusCode.OK;
+			res.StatusDescription = "OK";
 
-	log(ctx);
-}
+			string data = "Hello World";
+			byte[] buffer = Encoding.UTF8.GetBytes(data);
+			res.ContentLength64 = buffer.Length;
 
-void log(HttpListenerContext ctx)
-{
-	Console.Write($"[{ctx.Response.StatusCode}]  ");
-	Console.WriteLine(ctx.Request.Url);
+			using Stream ros = res.OutputStream;
+			ros.Write(buffer, 0, buffer.Length);
+
+			log(ctx);
+		}
+
+		void log(HttpListenerContext ctx)
+		{
+			Console.Write($"[{ctx.Response.StatusCode}]  ");
+			Console.WriteLine(ctx.Request.Url);
+		}
+	}
 }
