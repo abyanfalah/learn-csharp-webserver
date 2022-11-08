3710681c12dac0ed8597ff9837a854d3c260c25d
Abyan Falah <abyanf410@gmail.com>
2022-11-08 15:22:16 +0700

    text as reponse

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Net;
+
+
+Console.Clear();
+
+using var listener = new HttpListener();
+const string host = "http://localhost";
+const string port = "8000";
+const string uri = host + ":" + port + "/";
+listener.Prefixes.Add(uri);
+
+listener.Start();
+
+Console.WriteLine($"listeting to port {port}");
+
+while (true)
+{
+	HttpListenerContext ctx = listener.GetContext();
+	HttpListenerRequest req = ctx.Request;
+
+	using HttpListenerResponse res = ctx.Response;
+	// res.Headers.Set("Content-type", "application/json");
+	res.Headers.Set("Content-type", "text/plain");
+
+	res.StatusCode = (int)HttpStatusCode.OK;
+	res.StatusDescription = "OK";
+
+
+	string data = "Hello World";
+	byte[] buffer = Encoding.UTF8.GetBytes(data);
+	res.ContentLength64 = buffer.Length;
+
+	using Stream ros = res.OutputStream;
+	ros.Write(buffer, 0, buffer.Length);
+
+
+	log(ctx);
+}
+
+void log(HttpListenerContext ctx)
+{
+	Console.Write($"[{ctx.Response.StatusCode}]  ");
+	Console.WriteLine(ctx.Request.Url);
+}
