e83c3aea90559a4e14447c71036cf29e0be991f9
Abyan Falah <abyanf410@gmail.com>
2022-11-08 16:55:22 +0700

    asfd

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Net.Http;
+using System.Net;
+
+
+using var listener = new HttpListener();
+listener.Prefixes.Add("http://localhost:8000");
+
+listener.Start();
+
+while (true)
+{
+	HttpListenerContext ctx = listener.GetContext();
+	HttpListenerRequest req = ctx.Request;
+
+
+}
